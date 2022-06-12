a498c38fb40db29b3263b0d3dec76869c19e164a
SI-Kim <skim88.1942@gmail.com>
2022-06-12 17:09:40 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlinvuepractice/KotlinVuePracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinvuepractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinVuePracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinVuePracticeApplication>(*args)
+}
